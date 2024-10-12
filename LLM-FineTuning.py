dc83160a1ad27388d83f614acb13aff60fbbb3e2
Prashanth-shukla <147225621+Prashanth-shukla@users.noreply.github.com>
2024-10-12 08:22:23 +0530

    first commit

--- LLM-FineTuning.py
+++ LLM-FineTuning.py
@@ -31,27 +31,6 @@ num_labels = len(data['intent'])
 model = XLMRobertaForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
 
 
-training_args = TrainingArguments(
-    output_dir='./results',
-    evaluation_strategy='epoch',
-    learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=3,
-    weight_decay=0.01,
-)
-
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=test_dataset,
-)
-
-
-trainer.train()
-
 
 model.save('./fine-tuned-alexa')
 tokenizer.save('./fine-tuned-alexa')
